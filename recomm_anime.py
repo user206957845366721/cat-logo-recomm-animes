6431da7c588e2da739887faa0168754b4a460e0b
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 18:12:11 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -16,51 +16,23 @@ st.title("Olá!")
 st.write("Seja muito bem-vindo, ou vinda, ao meu site pessoal de recomendações de animes. Este site está sendo produzido para resguardar a criadora de recuperações e possíveis DPs. Espero que goste!")
 st.image(image="separador.png", use_column_width='auto')
 
-def one_piece():
-    rating= 5
-    full_stars = "⭐" * rating
-    empty_stars = "⭕" * (5 - rating)
-    stars = full_stars + empty_stars
-    st.title("One Piece")
-    st.write(stars)
-    st.image(image="one-piece.jpg", width=None)
-    st.write("Escrever sobre one piece aqui.")
+def home_page():
+    st.title("Home Page")
+    st.write("Welcome to the home page!")
 
-def megami_sama():
-    rating= 5
-    full_stars = "⭐" * rating
-    empty_stars = "⭕" * (5 - rating)
-    stars = full_stars + empty_stars
-    st.title("A! Megami Sama!")
-    st.write(stars)
-    st.image(image="megamisama.png", width=None)
-    st.write("Escrever sobre  aqui.")
+def about_page():
+    st.title("About Page")
+    st.write("This is the about page.")
 
-def taihou_shichauzo():
-    rating= 5
-    full_stars = "⭐" * rating
-    empty_stars = "⭕" * (5 - rating)
-    stars = full_stars + empty_stars
-    st.image(image="taihou-shichauzo.jpg", width=None)
-    st.title("Taihou Shichauzo/You're Under Arrest!")
-    st.write("Escrever sobre  aqui.")
+def contact_page():
+    st.title("Contact Page")
+    st.write("This is the contact page.")
 
-def akage_ann():
-    rating= 5
-    full_stars = "⭐" * rating
-    empty_stars = "⭕" * (5 - rating)
-    stars = full_stars + empty_stars
-    st.image(image="akage-no-ann.jpg", width=None)
-    st.title("Akage no Ann")
-    st.write("Escrever sobre  aqui.")
-    
-page = st.sidebar.selectbox("De qual anime você quer ler sobre?", ["One Piece", "A! Megami Sama!", "Taihou Shichauzo/You're Under Arrest!", "Akage no Ann"])
+page = st.sidebar.selectbox("Select a page", ["Home", "About", "Contact"])
 
-if page == "One Piece":
-    one_piece()
-elif page == "A! Megami Sama!":
-    megami_sama()
-elif page == "Taihou Shichauzo/You're Under Arrest!":
-    taihou_shichauzo()
-elif page == "Akage no Ann":
-    akage_ann()
+if page == "Home":
+    home_page()
+elif page == "About":
+    about_page()
+elif page == "Contact":
+    contact_page()
