c24c6ccd41b0189dfe36b9c9e8702f548dc25f5c
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 15:27:41 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -1,27 +1,10 @@
-# MEU PRIMEIRO WEB APP
 import streamlit as st
-import pandas as pd
+import pandas as np
 import numpy as np
-import requests
 
-with st.container():
-    st.image("Programação Engenharia Civil.png")
+st.setpage.config(
+    page_title="Catálogo de Recomendações de Anime Pessoal",
+)
 
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("Bem-vindo/a!")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("Site de Tabela de Orçamentos - Engenharia Civil 2024")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Selecione as opções desejadas abaixo:")
-
-left, right = st.columns(2)
-if left.button("Tabela", use_container_width=True):
-    left.markdown(st.dataframe(df1))
-if right.button("Gráfico", use_container_width=True):
-    right.markdown(st.dataframe(df2))
-
-# Use st.write("") para adicionar um texto ao seu Web app
-
-#usar excel. usar abas. cada aba tem dados de materiais, usar isso para comparar. e criar 3 pelo menos, das coisas que vamos usar
+st.title("Site Reserva, tô vendo que o grupo não tá se mexendo.")
+st.sidebar.success("Select a page above.")
