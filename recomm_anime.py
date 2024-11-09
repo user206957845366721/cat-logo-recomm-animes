fe7cb102b1c685ef176840736ff51012230cab59
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 21:04:08 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -15,8 +15,9 @@ st.set_page_config(
 st.title("Olá!")
 st.write("Seja muito bem-vindo, ou vinda, ao meu site pessoal de recomendações de animes. Este site está sendo produzido para resguardar a criadora de recuperações e possíveis DPs. Espero que goste!")
 st.image(image="separador.png", use_column_width='auto')
-st.subheader("Antes, uma breve introdução...)
-st.write("A história dos animes, e consequentemente dos mangás, começa há um certo tempo
+st.subheader("Antes, uma breve introdução...")
+st.write("A história dos animes, e consequentemente dos mangás, começa há um certo tempo.")
+st.image(image="separador.png", use_column_width='auto')
 
 def one_piece():
     rating= 5
