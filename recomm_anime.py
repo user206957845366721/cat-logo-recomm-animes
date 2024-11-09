098a389e9a1fa6d0dab7598e50dbeb7b759235b3
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 16:35:26 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -16,4 +16,4 @@ st.title("Olá!")
 st.write("Seja muito bem-vindo, ou vinda, ao meu site pessoal de recomendações de animes. Este site está sendo produzido para resguardar a criadora de recuperações e possíveis DPs. Espero que goste!")
 st.sidebar.success("Select a page above.")
 
-st.Page(page=Page1.py, title=teste, icon=None, url_path=None, default=False)
+st.Page(page="Page1.py", title=teste, icon=None, url_path="https://github.com/user206957845366721/cat-logo-recomm-animes/blob/main/Page1.py", default=False)
