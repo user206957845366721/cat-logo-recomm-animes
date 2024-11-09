3358765f58141f753a56f5e6128f17ad44403ce6
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 16:55:19 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -16,10 +16,17 @@ st.title("Olá!")
 st.write("Seja muito bem-vindo, ou vinda, ao meu site pessoal de recomendações de animes. Este site está sendo produzido para resguardar a criadora de recuperações e possíveis DPs. Espero que goste!")
 st.sidebar.success("Select a page above.")
 
-pages = {
-    "user206957845366721": [
-        st.Page("Page1.py", title="Teste"),
-    ]
-}
-pg = st.navigation(pages)
-pg.run()
+def teste_1():
+    st.title("Teste 1")
+    st.write("Teste.")
+
+def teste_2()
+st.title("Teste 2")
+st.write("Teste.")
+
+page = st.sidebar.selectbox("De qual anime você quer ler sobre?", ["Teste 1", "Teste 2"])
+
+if page == "Teste 1"
+    teste_1()
+elif page == "Teste 2"
+    teste_2()
