adf6a12f1c74c43675eabfea89eb49811555d65d
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 15:37:26 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as np
 import numpy as np
 
-st.setpage.config(
-    page_title="Catálogo de Recomendações de Anime Pessoal",
-)
+st.setpage.config(page_title="Catálogo de Recomendações de Anime Pessoal")
 
 st.title("Site Reserva, tô vendo que o grupo não tá se mexendo.")
 st.sidebar.success("Select a page above.")
