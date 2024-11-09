2bb07c1f79ee2feb5642f18d974dfe4ad3c1ab97
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 15:42:52 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -5,5 +5,9 @@ st.set_page_config(
     page_icon="✨",
     layout="wide",
     initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
