d408276a6e77b05f3c0f9ece238cd82d1e2eda77
user206957845366721 <2304040@escolas.anchieta.br>
2024-11-09 15:42:14 +0000

    Update recomm_anime.py

--- recomm_anime.py
+++ recomm_anime.py
@@ -1,13 +1,9 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Ex-stream-ly Cool App",
-    page_icon="🧊",
+    page_title="Site de Recomendações de Anime!",
+    page_icon="✨",
     layout="wide",
     initial_sidebar_state="expanded",
-    menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
     }
 )
